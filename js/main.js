9e1c3bc6c1b79af91b78ca729c90c1afcb8b50af
omer-kuluc <omerkuluc96@gmail.com>
2025-12-12 23:40:10 +0300

    süreler güncellendi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-gsap.to(".intro-text", { text: "WES ANDERSON", ease: "power4.in", opacity: 1, duration: 4, repeat: 1, yoyo: true });
-gsap.to(".intro-text", { text: "GRAND", ease: "power2.inOut", opacity: 1, duration: 1.5 }, "-=1.5");
+gsap.to(".intro-text", { text: "WES ANDERSON", ease: "power4.inOut", opacity: 1, duration: 3.75, repeat: 1, yoyo: true });
+gsap.to(".intro-text", { text: "GRAND", ease: "power2.inOut", opacity: 1, duration: 1.5 }, "-=1");
 gsap.to(".intro-text", { text: "BUDAPEST", ease: "power2.inOut", opacity: 1, duration: 1.5 }, "+=0.5");
 gsap.to(".intro-text", { text: "HOTEL", ease: "power2.inOut", opacity: 1, duration: 1.5 }, "+=0.5");
 
@@ -8,6 +8,6 @@ gsap.to(".intro-text", { text: "HOTEL", ease: "power2.inOut", opacity: 1, durati
 gsap.set(".effect", { autoAlpha: 1 })
 let tl = gsap.timeline()
   .from("h1", { scale: 0.2, duration: 4.5, ease: "power4.inOut" }, "+=3")
-  .to(".blendImage, .bg", { scale: 1, duration: 5.5 }, "+=3")
+  .to(".blendImage, .bg", { scale: 1, duration: 5.5 }, "+=2")
   .to(".dark", { opacity: 0, duration: 2 }, "-=0.75")
 
